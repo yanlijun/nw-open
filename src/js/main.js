744160c38bb1d166178ee23056e167f234c02b44
kevin yan <540565781@qq.com>
2016-07-27 10:40:41 +0800

    完成首页

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,32 @@ var jQuery = jQuery || window.jQuery;
     // if no jQuery library, then quit
     if (typeof $ === 'undefined') { return; }
 
-    console.log(123);
+    var $dialog = $('#dialog');
+    var $login = $('#login');
+    var $register = $('#register');
 
+    function stopPropagation(e) {
+        e.stopPropagation();
+    }
+    function showLogin() {
+        $dialog.addClass('show');
+        $login.addClass('crt').siblings().removeClass('crt');
+        return false;
+    }
+    function showRegister() {
+        $dialog.addClass('show');
+        $register.addClass('crt').siblings().removeClass('crt');
+        return false;
+    }
+    function hideDialog() {
+        $dialog.removeClass('show');
+    }
+
+    if ($dialog.length) {
+        $('.login').on('click', showLogin);
+        $('.register').on('click', showRegister);
+        $dialog.find('.m-pnl').on('click', stopPropagation);
+        $('.u-ico-close').on('click', hideDialog);
+        $(window).on('click', hideDialog);
+    }
 })(jQuery);
